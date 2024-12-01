9b4138533a096c9552de6c54c8ecdc1856e2d585
lewisbirks <22620804+lewisbirks@users.noreply.github.com>
2024-12-01 14:04:36 +0000

    refactor: use fewer java constructs

--- src/main/kotlin/Day.kt
+++ src/main/kotlin/Day.kt
@@ -1,6 +1,6 @@
 abstract class Day(private val day: Int, private val name: String) {
 
-    private val filename : String = "Day${String.format("%02d", day)}.txt"
+    private val filename: String = "Day${String.format("%02d", day)}.txt"
     protected var input: List<String> = readInput(filename)
 
     abstract fun part1(): Any
@@ -9,6 +9,32 @@ abstract class Day(private val day: Int, private val name: String) {
     protected open fun parse(): List<String> {
         return input
     }
+
+    fun process() {
+        fun buildPart(part: Any): String {
+            return if (part is String) {
+                part.lines().map { "║   $it" }.joinToString { "\n" }
+            } else {
+                "║   $part"
+            }
+        }
+
+        sequenceOf(
+            {
+                """
+                ╔════════════════════════════════════════════════
+                ║ $this
+                ╠════════════════════════════════════════════════
+                ║ Part 1:
+                """.trimIndent()
+            },
+            { buildPart(part1()) },
+            { "║ Part 2" },
+            { buildPart(part2()) },
+            { "╚════════════════════════════════════════════════" })
+            .forEach { it().println() }
+    }
+
 
     override fun toString(): String {
         return "Day ${String.format("%02d", day)}: $name"
--- src/main/kotlin/Days.kt
+++ src/main/kotlin/Days.kt
@@ -1,29 +1,7 @@
-import java.util.function.Supplier
-
 fun main() {
 
-    val days: List<Supplier<Day>> = listOf(Supplier { Day01() })
+    val days: List<() -> Day> = listOf({ Day01() })
 
-    days.map { it.get() }.forEach {
-        """
-            
-            ╔════════════════════════════════════════════════
-            ║ $it
-            ╠════════════════════════════════════════════════
-        """.trimIndent().println()
-        var part = it.part1()
-        printPart(part, "1")
-        part = it.part2()
-        printPart(part, "2")
-        "╚════════════════════════════════════════════════".println()
-    }
+    days.map { it() }.forEach { it.process() }
 }
 
-fun printPart(part: Any, num: String) {
-    "║ Part $num:".println()
-    if (part is String) {
-        part.lines().map { "║   $it" }.forEach { it.println() }
-    } else {
-        "║   $part".println()
-    }
-}
