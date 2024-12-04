ae252e337d4af15107e29a21be644ee4b09ee840
lewisbirks <lewisb@kainos.com>
2024-12-04 14:17:55 +0000

    impr: precompute `A` locations first when parsing, like being done with `X`

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -1,17 +1,26 @@
 class Day04 : Day(4, "Ceres Search") {
 
     private val lines: List<String> = parse()
-    private val xs: List<Point> = buildList {
+    private val xLocations: List<Point>
+    private val aLocations: List<Point>
+    private val maxY: Int = lines.size - 1
+    private val maxX: Int = lines[0].length - 1
+
+    init {
+        val xLocations = mutableListOf<Point>()
+        val aLocations = mutableListOf<Point>()
         lines.mapIndexed { y, line ->
             line.forEachIndexed { x, value ->
                 if (value == 'X') {
-                    add(Point(x, y))
+                    xLocations.add(Point(x, y))
+                } else if (value == 'A') {
+                    aLocations.add(Point(x, y))
                 }
             }
         }
+        this.xLocations = xLocations
+        this.aLocations = aLocations
     }
-    private val maxY: Int = lines.size - 1
-    private val maxX: Int = lines[0].length - 1
 
     override fun part1(): Any {
         val toFind = listOf('M', 'A', 'S')
@@ -27,54 +36,43 @@ class Day04 : Day(4, "Ceres Search") {
             return true
         }
 
-        return xs.sumOf { x -> Direction.entries.count { direction -> valid(direction, x) } }
+        return xLocations.sumOf { x -> Direction.entries.count { direction -> valid(direction, x) } }
     }
 
     override fun part2(): Any {
-        var count = 0
-        for (y in 0..(maxY - 2)) {
-            for (x in 0..(maxY - 2)) {
-                val middle = Point(x + 1, y + 1)
-                if (lines[middle.y][middle.x] != 'A') {
-                    continue
-                }
-                val topLeft = middle.move(Direction.UP_LEFT)
-                val topRight = middle.move(Direction.UP_RIGHT)
-                val bottomLeft = middle.move(Direction.DOWN_LEFT)
-                val bottomRight = middle.move(Direction.DOWN_RIGHT)
+        fun valid(a: Point): Boolean {
+            val topLeft = a.neighbour(Direction.UP_LEFT, maxX, maxY) ?: return false
+            val topRight = a.neighbour(Direction.UP_RIGHT, maxX, maxY) ?: return false
+            val bottomLeft = a.neighbour(Direction.DOWN_LEFT, maxX, maxY) ?: return false
+            val bottomRight = a.neighbour(Direction.DOWN_RIGHT, maxX, maxY) ?: return false
 
-                fun check(topLeftChar: Char, topRightChar: Char, bottomLeftChar: Char, bottomRightChar: Char): Boolean = lines[topLeft.y][topLeft.x] == topLeftChar
-                        && lines[bottomLeft.y][bottomLeft.x] == bottomLeftChar
-                        && lines[topRight.y][topRight.x] == topRightChar
-                        && lines[bottomRight.y][bottomRight.x] == bottomRightChar
+            fun check(topLeftChar: Char, topRightChar: Char, bottomLeftChar: Char, bottomRightChar: Char): Boolean = lines[topLeft.y][topLeft.x] == topLeftChar
+                    && lines[bottomLeft.y][bottomLeft.x] == bottomLeftChar
+                    && lines[topRight.y][topRight.x] == topRightChar
+                    && lines[bottomRight.y][bottomRight.x] == bottomRightChar
 
-
-                // M S | S M
-                //  A  |  A
-                // M S | S M
-                // M M | S S
-                //  A  |  A
-                // S S | M M
-                if (check('M', 'S', 'M', 'S')
-                    || check('S', 'M', 'S', 'M')
-                    || check('M', 'M', 'S', 'S')
-                    || check('S', 'S', 'M', 'M')
-                ) {
-                    count++
-                }
-            }
+            // |-----|-----|
+            // | M S | S M |
+            // |  A  |  A  |
+            // | M S | S M |
+            // |-----|-----|
+            // | M M | S S |
+            // |  A  |  A  |
+            // | S S | M M |
+            // |-----|-----|
+            return check('M', 'S', 'M', 'S') || check('S', 'M', 'S', 'M')
+                    || check('M', 'M', 'S', 'S') || check('S', 'S', 'M', 'M')
         }
 
-        return count
+        return aLocations.count { valid(it) }
     }
-
 
     data class Point(val x: Int, val y: Int) {
         fun neighbour(direction: Direction, maxX: Int, maxY: Int): Point? {
             return move(direction).takeUnless { it.x < 0 || it.y < 0 || it.x > maxX || it.y > maxY }
         }
 
-        fun move(direction: Direction): Point {
+        private fun move(direction: Direction): Point {
             return when (direction) {
                 Direction.LEFT -> Point(x - 1, y)
                 Direction.RIGHT -> Point(x + 1, y)
