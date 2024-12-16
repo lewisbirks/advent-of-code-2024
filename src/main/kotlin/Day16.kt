699ddafaa9a517d3edd1a83ab70abb4da0d13568
lewisbirks <lewisb@kainos.com>
2024-12-16 11:23:20 +0000

    chore: init day

---
+++ src/main/kotlin/Day16.kt
@@ -0,0 +1,14 @@
+class Day16 : Day(16, "Reindeer Maze") {
+
+    override fun part1(): Any = TODO()
+
+    override fun part2(): Any = TODO()
+}
+
+fun main() {
+    val day = Day16()
+    println(day)
+//    println(day.part1())
+//    println(day.part2())
+//    println(DayXX().process())
+}
