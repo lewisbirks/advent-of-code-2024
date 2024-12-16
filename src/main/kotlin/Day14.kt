0e12d5c755be6959f275ff1d1afafe8c9517244c
lewisbirks <lewisb@kainos.com>
2024-12-16 11:21:54 +0000

    chore: init day

---
+++ src/main/kotlin/Day14.kt
@@ -0,0 +1,14 @@
+class Day14 : Day(14, "Restroom Redoubt") {
+
+    override fun part1(): Any = TODO()
+
+    override fun part2(): Any = TODO()
+}
+
+fun main() {
+    val day = Day14()
+    println(day)
+//    println(day.part1())
+//    println(day.part2())
+//    println(DayXX().process())
+}
---
+++ src/test/kotlin/Day14Test.kt
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class Day14Test {
+
+    private val underTest = Day14()
+
+    @Test
+    fun part1() {
+        assertEquals(-1, underTest.part1())
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(-1, underTest.part2())
+    }
+}
