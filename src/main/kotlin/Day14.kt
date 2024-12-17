2c08c370ac3ecdadd01f69d9d989d7a6880ed208
lewisbirks <lewisb@kainos.com>
2024-12-17 11:24:44 +0000

    feat: day 14 part 1

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,14 +1,137 @@
 class Day14 : Day(14, "Restroom Redoubt") {
 
-    override fun part1(): Any = TODO()
+    private val initial: List<Robot> = parse().map { Robot.parse(it) }
 
-    override fun part2(): Any = TODO()
+    var maxX: Int = 101
+    var maxY: Int = 103
+
+    override fun part1(): Any {
+        val map = Map(initial, maxX, maxY)
+        map.moveRobots(100)
+
+        return map.safety()
+    }
+
+    override fun part2(): Any {
+//        val map = Map(initial, maxX, maxY)
+//        var i = 0
+//        fun printRobots() {
+//            val entries = map.robots.map{ it.position }.groupBy { it }.map { it.key to it.value.size.toString() }.associate { it }
+//
+//            println("${"\n".repeat(maxY + 2)}Iteration: $i")
+//            for (y in 0..<maxY) {
+//                for (x in 0..<maxX) {
+//                    val p = entries.getOrDefault(Point(x, y), ".")
+//                    print(p)
+//                }
+//                println()
+//            }
+//        }
+//
+//        while (true) {
+//            printRobots()
+//            map.moveRobots(1)
+//            i++
+//            Thread.sleep(500)
+//        }
+//
+//        return "nope"
+        TODO()
+    }
+
+
+    private class Map(robots: List<Robot>, private val maxX: Int, private val maxY: Int) {
+        val robots: List<Robot> = robots.map { it.copy() }
+
+        fun moveRobots(times: Int) = this.robots.forEach { it.move(times, maxX, maxY) }
+
+        fun safety(): Long {
+            // `isValid` is inclusive so -1 here to ensure that things work as expected
+            val maxX = this.maxX - 1
+            val maxY = this.maxY - 1
+            val leftHalfEnd = (maxX / 2) - 1
+            val topHalfEnd = (maxY / 2) - 1
+            val rightHalfStart = maxX - leftHalfEnd
+            val bottomHalfStart = maxY - topHalfEnd
+
+            var topLeft = 0L
+            var topRight = 0L
+            var bottomLeft = 0L
+            var bottomRight = 0L
+
+            // A | B
+            // -----
+            // C | D
+            for (robot in robots) {
+                val position = robot.position
+                when {
+                    // A
+                    position.isValid(leftHalfEnd, topHalfEnd) -> topLeft++
+                    // B
+                    position.isValid(maxX, topHalfEnd, rightHalfStart, 0) -> topRight++
+                    // C
+                    position.isValid(leftHalfEnd, maxY, 0, bottomHalfStart) -> bottomLeft++
+                    // D
+                    position.isValid(maxX, maxY, rightHalfStart, bottomHalfStart) -> bottomRight++
+                }
+            }
+            return topLeft * topRight * bottomLeft * bottomRight
+        }
+    }
+
+
+    private class Robot private constructor(var position: Point, private val velocity: Velocity, private val id: Int){
+        constructor(position: Point, velocity: Velocity): this(position, velocity, ++counter)
+
+        fun copy(): Robot = Robot(position, velocity, id)
+
+        fun move(times: Int, maxX: Int, maxY: Int) {
+            val velocity = this.velocity * times
+            var (x, y) = velocity.addTo(position)
+
+            x %= maxX
+            y %= maxY
+
+            if (x < 0) x += maxX
+            if (y < 0) y += maxY
+
+            this.position = Point(x, y)
+        }
+
+
+        override fun toString(): String {
+            return "$id => $position"
+        }
+
+        companion object {
+            private var counter: Int = 0
+
+            fun parse(line: String): Robot {
+                val (unparsedP, unparsedV) = line.split(' ')
+                fun process(to: String): Pair<Int, Int> {
+                    val (x,y) = to.substring(2).split(',').map { it.toInt() }
+                    return (x to y)
+                }
+                val position = process(unparsedP).let { (x,y) -> Point(x, y) }
+
+                val velocity = process(unparsedV).let { (x,y) -> Velocity(x, y) }
+
+                return Robot(position, velocity)
+            }
+        }
+    }
+
+    private data class Velocity(val x: Int, val y:Int) {
+        fun addTo(position: Point): Point = Point(position.x + this.x, position.y + this.y)
+
+        operator fun times(a: Int): Velocity = Velocity(x * a, y * a)
+    }
 }
 
 fun main() {
     val day = Day14()
     println(day)
-//    println(day.part1())
-//    println(day.part2())
+    println(day.part1())
+    println(day.part2())
 //    println(DayXX().process())
 }
--- src/test/kotlin/Day14Test.kt
+++ src/test/kotlin/Day14Test.kt
@@ -1,15 +1,24 @@
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class Day14Test {
 
     private val underTest = Day14()
 
-    @Test
-    fun part1() {
-        assertEquals(-1, underTest.part1())
+    @BeforeEach
+    fun setUp() {
+        underTest.maxX = 11
+        underTest.maxY = 7
     }
 
+    @Test
+    fun part1() {
+        assertEquals(12L, underTest.part1())
+    }
+
+    @Disabled
     @Test
     fun part2() {
         assertEquals(-1, underTest.part2())
